059c5ff99c649bcc981751e177b5e512d6ff9101
pta201 <pta20801@gmail.com>
2024-11-20 15:04:26 +0700

    style: add basic responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   };
   return (
     <div className="flex items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex  gap-16 row-start-2 items-center sm:items-start">
+      <main className="flex flex-col md:flex-row  gap-16 row-start-2 items-center sm:items-start">
         <Form onSubmit={handleSubmit} />
         <Chart
           cooperate={chartData.cooperate}
